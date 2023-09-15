ad8c3498c79654d6092846098e13fef05c5034f1
Snap <XxSnapperGeexX@gmail.com>
2023-09-14 21:43:09 -0700

    Remove scss content path from tailwind config.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import {resolve as resolvePath } from "node:path";
 
 export default {
-    content: [resolvePath(".", "src", "main", "scss", "*.scss"), resolvePath(".", "src", "main", "html", "*.html")],
+    content: [resolvePath(".", "src", "main", "html", "*.html")],
     theme: {
         extend: {},
     },
