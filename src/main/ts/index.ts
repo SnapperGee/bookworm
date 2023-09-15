2509a7e50d39c686c4c4e7b5466f15d4bb8ab50a
Snap <XxSnapperGeexX@gmail.com>
2023-09-14 20:44:00 -0700

    Deleting unused API from index ts page.

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,21 +1,9 @@
-const OMDB_API_KEY = "afecf7e1";
-
 const NY_TIMES_API_KEY = "JrHXn4ufVldkRPHOEsBzG3wUMFHtGn1M";
 
-const movieTitle = "swordfish";
-
-fetch(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&t=${movieTitle}`)
-    .then(response => response.json())
-    .then(placesData => console.log(placesData));
-
 fetch("https://openlibrary.org/search.json?q=the+lord+of+the+rings.json")
-    .then(response => response.json())
-    .then(omdbData => console.log(omdbData));
+    .then(openLibResponse => openLibResponse.json())
+    .then(openLibResponse => console.log(openLibResponse));
 
 fetch(`https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key=${NY_TIMES_API_KEY}`)
-    .then(response => response.json())
-    .then(bookData => console.log(bookData));
-
-fetch("https://openlibrary.org/search.json?title=the+lord+of+the+rings")
-    .then(response => response.json())
-    .then(bookData => console.log(bookData));
+    .then(nyTimesResponse => nyTimesResponse.json())
+    .then(nyTimesData => console.log(nyTimesData));
