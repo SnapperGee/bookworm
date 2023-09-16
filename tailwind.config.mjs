1458370c1c9b335dada3e1ea49eef698b08e23db
Snap <XxSnapperGeexX@gmail.com>
2023-09-15 21:13:49 -0700

    Make output build correctly.

--- tailwind.config.mjs
+++
@@ -1,9 +0,0 @@
-import {resolve as resolvePath } from "node:path";
-
-export default {
-    content: [resolvePath(".", "src", "main", "html", "*.html")],
-    theme: {
-        extend: {},
-    },
-    plugins: []
-}
--- webpack.config.mjs
+++ webpack.config.mjs
@@ -6,17 +6,23 @@ import autoprefixer from "autoprefixer";
 
 export default function(env, argv) {
     return {
-        entry: [resolvePath('.', 'src', 'main', 'ts', 'index.ts'), resolvePath('.', 'src', 'main', 'scss', 'index.scss')],
-        mode: env.production ? 'production' : 'development',
-        devtool: env.production ? 'source-map' : 'eval-source-map',
+        entry:
+        {
+            genres: [resolvePath(".", "src", "main", "ts", "genres.ts"), resolvePath(".", "src", "main", "scss", "genres.scss")],
+            index: [resolvePath(".", "src", "main", "ts", "index.ts"), resolvePath(".", "src", "main", "scss", "index.scss")],
+            readinglist: [resolvePath(".", "src", "main", "ts", "readinglist.ts"), resolvePath(".", "src", "main", "scss", "readinglist.scss")],
+            recommendations: [resolvePath(".", "src", "main", "ts", "recommendations.ts"), resolvePath(".", "src", "main", "scss", "recommendations.scss")]
+        },
+        mode: env.production ? "production" : "development",
+        devtool: env.production ? "source-map" : "eval-source-map",
         module: {
             rules: [
                 {
                     test: /\.ts?$/,
                     use: [
                         {
-                            loader: 'ts-loader',
-                            options: { configFile: resolvePath('.', 'config', env.production ? 'tsconfig.dist.json' : 'tsconfig.dev.json') }
+                            loader: "ts-loader",
+                            options: { configFile: resolvePath(".", "config", env.production ? "tsconfig.dist.json" : "tsconfig.dev.json") }
                         }
                     ],
                     exclude: /node_modules/,
@@ -24,10 +30,10 @@ export default function(env, argv) {
                 {
                     test: /\.s[ac]ss$/,
                     use: [
-                        env.production ? MiniCssExtractPlugin.loader : 'style-loader',
-                        'css-loader',
+                        env.production ? MiniCssExtractPlugin.loader : "style-loader",
+                        "css-loader",
                         {
-                            loader: 'postcss-loader',
+                            loader: "postcss-loader",
                             options: {
                                 postcssOptions: {
                                     plugins: [
@@ -38,22 +44,22 @@ export default function(env, argv) {
                                 }
                             }
                         },
-                        'sass-loader'
+                        "sass-loader"
                     ]
                 },
                 {
                     test: /\.html$/,
-                    loader: 'html-loader'
+                    loader: "html-loader"
                 },
                 {
                     test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                    type: 'asset/resource'
+                    type: "asset/resource"
                 }
             ],
         },
         devServer: {
             static: {
-                directory: resolvePath('.', 'build')
+                directory: resolvePath(".", "build")
             },
             client: {
                 overlay: false
@@ -62,17 +68,41 @@ export default function(env, argv) {
             port: 9908,
             hot: true
         },
-        resolve: { extensions: ['.tsx', '.ts', '.js'], },
+        resolve: { extensions: [".ts", ".js"], },
         output: {
-            filename: env.production ? 'index.bundle.min.mjs' : 'index.bundle.mjs',
-            path: resolvePath('.', 'build'),
+            filename: env.production ? "[name].bundle.min.mjs" : "[name].bundle.mjs",
+            path: resolvePath(".", "build"),
             clean: true
         },
         plugins: [
             new MiniCssExtractPlugin({ filename: "index.min.css" }),
             new HtmlWebpackPlugin({
-                template: resolvePath('.', 'src', 'main', 'html', 'index.html'),
-                scriptLoading: 'module'
+                filename: "genres.html",
+                template: resolvePath(".", "src", "main", "html", "genres.html"),
+                scriptLoading: "module",
+                chunks: ["genres"],
+                excludeChunks: ["index", "readinglist", "recommendations"]
+            }),
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: resolvePath(".", "src", "main", "html", "index.html"),
+                scriptLoading: "module",
+                chunks: ["index"],
+                excludeChunks: ["genres", "readinglist", "recommendations"]
+            }),
+            new HtmlWebpackPlugin({
+                filename: "readinglist.html",
+                template: resolvePath(".", "src", "main", "html", "readinglist.html"),
+                scriptLoading: "module",
+                chunks: ["readinglist"],
+                excludeChunks: ["index", "genres", "recommendations"]
+            }),
+            new HtmlWebpackPlugin({
+                filename: "recommendations.html",
+                template: resolvePath(".", "src", "main", "html", "recommendations.html"),
+                scriptLoading: "module",
+                chunks: ["recommendations"],
+                excludeChunks: ["index", "genres", "readinglist"]
             })
         ]
     }
