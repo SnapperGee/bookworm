74596982242c6cef62f97a079c272792bd7b5ae6
Snap <XxSnapperGeexX@gmail.com>
2023-09-19 11:32:58 -0700

    Create CheckboxCollection objects for topic and subject checkboxes.

--- src/main/ts/genre-recommendations.ts
+++ src/main/ts/genre-recommendations.ts
@@ -1,6 +1,12 @@
 import { createNavbar } from "./navbar";
+import { getTopicCheckboxes, getSubjectCheckboxes } from "./genre-recommendations/dom";
+import { type CheckboxCollection, checkboxCollection as createCheckboxCollection } from "./genre-recommendations/checkboxCollection";
 
 const bodyHTML: HTMLElement = document.querySelector('body') as HTMLElement;
 bodyHTML.prepend(createNavbar());
 
-console.log("Recommendations");
+const topicCheckboxes: HTMLCollectionOf<HTMLInputElement> = getTopicCheckboxes();
+const subjectCheckboxes: HTMLCollectionOf<HTMLInputElement> = getSubjectCheckboxes();
+
+const topicCheckboxCollection: CheckboxCollection = createCheckboxCollection(topicCheckboxes);
+const subjectCheckboxCollection: CheckboxCollection = createCheckboxCollection(subjectCheckboxes);
