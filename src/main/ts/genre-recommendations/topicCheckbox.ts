c7f2a9aa4f7454bd75f8e758c1fe9f6af78e2e38
Snap <XxSnapperGeexX@gmail.com>
2023-09-19 17:00:53 -0700

    Add HTMLSelectElement parameter and comments to topicCheckboxEventFunction.

--- src/main/ts/genre-recommendations/topicCheckbox.ts
+++ src/main/ts/genre-recommendations/topicCheckbox.ts
@@ -1,6 +1,16 @@
-export const assignHTMLInputElementCheckChangeFunction = (
+/**
+ * Function that associates `HTMLInputElement` checkboxes to toggle the visibility of `HTMLFieldsetElement`s.
+ *
+ * @param checkboxHTMLInputElements The collection of `HTMLInputElement` checkboxes to check for checked/unchecked.
+ *
+ * @param htmlFieldsetElements The collection of `HTMLFieldsetElement`s to make visible/hidden.
+ *
+ * @param htmlSelectElement The `HTMLSelectElement` to override whether to make a fieldset visible or not.
+ */
+export const topicCheckboxEventFunction = (
     checkboxHTMLInputElements: HTMLCollectionOf<HTMLInputElement>,
-    htmlFieldsetElements: HTMLCollectionOf<HTMLFieldSetElement> ): void =>
+    htmlFieldsetElements: HTMLCollectionOf<HTMLFieldSetElement>,
+    htmlSelectElement: HTMLSelectElement ): void =>
 {
     for (let index = 0; index < checkboxHTMLInputElements.length; ++index)
     {
@@ -9,13 +19,15 @@ export const assignHTMLInputElementCheckChangeFunction = (
         topicCheckboxHTMLInputElement?.addEventListener("change", () => {
             const topicFieldset: HTMLFieldSetElement | null = htmlFieldsetElements.namedItem(topicCheckboxHTMLInputElement.name);
 
-            if (topicCheckboxHTMLInputElement.checked === true)
+            // If checkbox for topic is checked and visibility is set to "show", make topic field set visible
+            if (topicCheckboxHTMLInputElement.checked === true && htmlSelectElement.value === "show")
             {
                 if (topicFieldset !== null && topicFieldset.classList.contains("hidden"))
                 {
                     topicFieldset.classList.remove("hidden")
                 }
             }
+            // If checkbox is unchecked make topic fieldset hidden
             else
             {
                 if (topicFieldset !== null && ! topicFieldset.classList.contains("hidden"))
