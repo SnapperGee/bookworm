001702bfbd9e4be233f3ec4fc3cfb03f90529a6b
Snap <XxSnapperGeexX@gmail.com>
2023-09-19 17:41:49 -0700

    Create topicVisibilityDropdown module with function and use in genre-recommendations.ts.

--- src/main/ts/genre-recommendations.ts
+++ src/main/ts/genre-recommendations.ts
@@ -1,13 +1,15 @@
 import { createNavbar } from "./navbar";
 import { geVisibilityDropdown, getTopicCheckboxes, getTopicFieldsets } from "./genre-recommendations/dom";
 import { topicCheckboxEventFunction } from "./genre-recommendations/topicCheckbox";
+import { topicVisibilityDropdownEventFunction } from "./genre-recommendations/topicVisibilityDropdown";
 
 const bodyHTML: HTMLElement = document.querySelector('body') as HTMLElement;
 bodyHTML.prepend(createNavbar());
 
-const visibilityDropdown: HTMLSelectElement = geVisibilityDropdown();
+const visibilitySelectDropdown: HTMLSelectElement = geVisibilityDropdown();
 const topicCheckboxes: HTMLCollectionOf<HTMLInputElement> = getTopicCheckboxes();
 // const subjectCheckboxes: HTMLCollectionOf<HTMLInputElement> = getSubjectCheckboxes();
 const topicFieldsets: HTMLCollectionOf<HTMLFieldSetElement> = getTopicFieldsets();
 
-topicCheckboxEventFunction(topicCheckboxes, topicFieldsets, visibilityDropdown);
+topicVisibilityDropdownEventFunction(visibilitySelectDropdown, topicCheckboxes, topicFieldsets);
+topicCheckboxEventFunction(topicCheckboxes, topicFieldsets, visibilitySelectDropdown);
