4cb9bbbc7094fc9863320eff14a2644efd66459d
Snap <XxSnapperGeexX@gmail.com>
2023-09-19 22:00:27 -0700

    Remove author alt names from OpenLibDoc interface.

--- src/main/ts/genre-recommendations/openLibAPI.ts
+++ src/main/ts/genre-recommendations/openLibAPI.ts
@@ -3,7 +3,6 @@ const rootUrl: string = "https://openlibrary.org/search.json?";
 export interface OpenLibDoc
 {
     author_name: string[],
-    author_alternative_name: string[],
     title: string,
     subject: string[],
     cover_i: number
