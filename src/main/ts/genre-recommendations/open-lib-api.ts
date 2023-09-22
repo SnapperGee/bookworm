eb97dc82366d948ed49994e4c2dea8bb67513820
Snap <XxSnapperGeexX@gmail.com>
2023-09-21 19:28:45 -0700

    Add openLibDocEquals(OpenLibDoc, OpenLibDoc): boolean.

--- src/main/ts/genre-recommendations/open-lib-api.ts
+++ src/main/ts/genre-recommendations/open-lib-api.ts
@@ -24,4 +24,23 @@ export const enum CoverImgSize
     LARGE = "L"
 }
 
+export const openLibDocEquals = (anOpenLibDoc: OpenLibDoc, anotherOpenLibDoc: OpenLibDoc | undefined | null): boolean =>
+{
+    if (anotherOpenLibDoc === undefined || anotherOpenLibDoc === null)
+    {
+        return false;
+    }
+
+    if (   anOpenLibDoc.title !== anotherOpenLibDoc.title
+        || anOpenLibDoc.cover_i !== anotherOpenLibDoc.cover_i
+        || anOpenLibDoc.author_name.length !== anotherOpenLibDoc.author_name.length
+        || anOpenLibDoc.subject.length !== anotherOpenLibDoc.subject.length )
+    {
+        return false;
+    }
+
+    return    anOpenLibDoc.author_name.every(authorName => anotherOpenLibDoc.author_name.includes(authorName))
+           && anOpenLibDoc.subject.every(subj => anotherOpenLibDoc.subject.includes(subj));
+};
+
 export const createOpenLibBookCoverImgUrl = (coverImgID: number, imgSize: CoverImgSize): string => `https://covers.openlibrary.org/b/id/${coverImgID}-${imgSize}.jpg`;
