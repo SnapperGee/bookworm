80eb3a297d7eba13e0a35187d68c3f88952564b6
astro <angelica.strong0725@gmail.com>
2023-09-23 22:34:02 -0700

    edits lists and colors

--- src/main/ts/bookshelf/editshelfModal.ts
+++ src/main/ts/bookshelf/editshelfModal.ts
@@ -15,6 +15,8 @@ export function editModalHandler(open: boolean | null = null) {
     }
 }
 
+let currentEditingPlaylist: HTMLElement | null = null;
+
 document.addEventListener("DOMContentLoaded", function() {
     document.body.addEventListener('click', (event) => {
         const target = event.target as HTMLElement;
@@ -22,6 +24,53 @@ document.addEventListener("DOMContentLoaded", function() {
         if (target.classList.contains('edit-list') || target.parentElement?.classList.contains('edit-list')) {
             console.log('Modal opened');
             editModalHandler(true);
+
+            let containerElement: HTMLElement | null = target;
+
+            while (containerElement && containerElement.id !== 'playlist-container') {
+                containerElement = containerElement.parentElement as HTMLElement;
+            }
+            if (containerElement) {
+                const playlistNameElement = containerElement.querySelector('h3') as HTMLHeadElement;
+    
+                if (playlistNameElement) {
+                    currentEditingPlaylist = containerElement;
+                }
+            }
+        }
+    });
+    
+    const confirmBtn = document.getElementById('confirm');
+    confirmBtn?.addEventListener('click', (event) => {
+        event.stopPropagation();
+        if (currentEditingPlaylist) {
+            // Get new values from the modal
+            const newName = (document.getElementById('editName') as HTMLInputElement).value;
+            const newColor = (document.getElementById('editColor') as HTMLSelectElement).value;
+
+            // Update the list's name
+            const playlistNameElement = currentEditingPlaylist.querySelector('h3') as HTMLHeadingElement;
+            if (playlistNameElement) {
+                playlistNameElement.textContent = newName;
+            }
+
+             // Update the list's color
+            const playlistHeaderElement = currentEditingPlaylist.querySelector('div[id^="header-color-"]') as HTMLDivElement;
+            if (playlistHeaderElement) {
+            // First, remove any existing bg-* classes
+            for (const className of Array.from(playlistHeaderElement.classList)) {
+                    if (className.startsWith('bg-')) {
+                    playlistHeaderElement.classList.remove(className);
+                    break;
+                }
+            }
+            // Then, add the new color class
+            playlistHeaderElement.classList.add(newColor);
+        }
+
+            // Close the modal
+            console.log('Confirm button clicked')
+            editModalHandler(false);
         }
     });
 
--- src/main/ts/bookshelf/newshelf.ts
+++ src/main/ts/bookshelf/newshelf.ts
@@ -1,11 +1,14 @@
-export const createNewShelf = (shelfName: string, colorClass: string): HTMLDivElement => {
+export const createNewShelf = (shelfId: string, shelfName: string, colorClass: string): HTMLDivElement => {
 
     const playlistContainer: HTMLDivElement = document.createElement('div') as HTMLDivElement;
-        playlistContainer.classList.add('flex', 'flex-col', 'justify-center', 'm-5');
-        playlistContainer.id = 'playlist-container';
+        playlistContainer.classList.add('shelfId', 'flex', 'flex-col', 'justify-center', 'm-5');
+        const playlistContainerId = shelfId || ('shelfId' + new Date().getTime());
+        playlistContainer.id = playlistContainerId;
 
     const playlistHeader: HTMLDivElement = document.createElement('div');
         playlistHeader.classList.add(colorClass, 'rounded-lg', 'pb-1', 'w-full', 'flex', 'items-center', 'justify-between');
+        const playlistHeaderId = 'header-color-' + new Date().getTime();
+        playlistHeader.id = playlistHeaderId;
     
     const playlistName: HTMLHeadingElement = document.createElement('h3');
         playlistName.classList.add('inline-block', 'm-5', 'text-white', 'font-bold', 'text-xl');
