335d9d70a6932640463379015cde7b72ae243ed4
Snap <XxSnapperGeexX@gmail.com>
2023-09-24 23:51:15 -0700

    Refactor navbar typescript.

--- src/main/ts/navbar.ts
+++ src/main/ts/navbar.ts
@@ -2,15 +2,8 @@ import logoImgPath from "../resource/img/bookworm-transparent.png";
 
 export const createNavbar = (): HTMLDivElement => {
 
-    const headerContainer: HTMLDivElement = document.createElement('nav') as HTMLDivElement;
-    headerContainer.classList.add('w-full', 'bg-green');
-    headerContainer.id = 'header';
-
-    const navBlock: HTMLDivElement = document.createElement('div');
-    navBlock.classList.add('w-full', 'container','mx-auto','flex','flex-wrap','items-center', 'justify-between', 'mt-0', 'p-2', 'lg:py-6');
-
     const brandContainer: HTMLDivElement = document.createElement('div');
-    brandContainer.classList.add('pl-4', 'flex', 'items-center')
+    brandContainer.classList.add('flex', 'items-center')
 
     const logoImage: HTMLImageElement = document.createElement('img');
     logoImage.width = 125;
@@ -26,15 +19,9 @@ export const createNavbar = (): HTMLDivElement => {
     logoWormAnchor.classList.add('text-white', 'font-bold', 'text-4xl');
     logoWormAnchor.textContent = 'Worm';
 
-    const invisibleBlock: HTMLDivElement = document.createElement('div');
-    invisibleBlock.classList.add('w-full', 'lg:flex', 'lg:w-auto', 'mt-2', 'lg:mt-0', 'p-4', 'lg:p-0');
-    invisibleBlock.id = 'nav-content';
-
-    const navList: HTMLUListElement = document.createElement('ul');
-    navList.classList.add('list-reset', 'lg:flex', 'justify-end', 'flex-1', 'items-center');
-
-    const navLinks: HTMLLIElement = document.createElement('li');
-    navLinks.classList.add('mr-3');
+    brandContainer.appendChild(logoImage);
+    brandContainer.appendChild(logoBookAnchor);
+    brandContainer.appendChild(logoWormAnchor);
 
     const home: HTMLAnchorElement = document.createElement('a');
     home.classList.add('inline-block', 'text-white', 'p-4', 'text-xl')
@@ -47,7 +34,7 @@ export const createNavbar = (): HTMLDivElement => {
     bestSellers.textContent = 'Bestsellers';
 
     const forYou: HTMLAnchorElement = document.createElement('a');
-    forYou.classList.add('inline-block', 'text-white', 'p-4', 'text-xl');
+    forYou.classList.add('inline-block', 'text-white', 'p-4', 'text-xl', 'whitespace-nowrap');
     forYou.href = './for-you.html';
     forYou.textContent = 'For You';
 
@@ -66,18 +53,20 @@ export const createNavbar = (): HTMLDivElement => {
         }
     }
 
-    headerContainer.appendChild(navBlock);
-    navBlock.appendChild(brandContainer);
-    brandContainer.appendChild(logoImage);
-    brandContainer.appendChild(logoBookAnchor);
-    brandContainer.appendChild(logoWormAnchor);
-    navBlock.appendChild(invisibleBlock);
-    navBlock.appendChild(navList);
-    navList.appendChild(navLinks);
+    const navLinks = document.createElement("div");
+    navLinks.classList.add('flex', 'flex-nowrap');
+
     navLinks.appendChild(home);
     navLinks.appendChild(bestSellers);
     navLinks.appendChild(forYou);
     navLinks.appendChild(bookshelf);
+
+    const headerContainer: HTMLDivElement = document.createElement('nav') as HTMLDivElement;
+    headerContainer.classList.add('flex', 'flex-wrap', 'justify-between', 'items-center', 'w-full', 'bg-green', 'px-8');
+    headerContainer.id = 'header';
+
+    headerContainer.appendChild(brandContainer);
+    headerContainer.appendChild(navLinks);
 
     return headerContainer;
 }
